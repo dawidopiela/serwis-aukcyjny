e4e1aaa069252c27f2cef0a6cbf53f7eec7fc646
Dariusz Kaszuba <dariusz.kaszuba11@gmail.com>
2019-04-02 09:26:29 +0200

    start

---
+++ src/main/java/pl/sa/serwisaukcyjny/SerwisaukcyjnyApplication.java
@@ -0,0 +1,13 @@
+package pl.sa.serwisaukcyjny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisaukcyjnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisaukcyjnyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sa/serwisaukcyjny/SerwisaukcyjnyApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sa.serwisaukcyjny;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerwisaukcyjnyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
