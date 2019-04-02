929a2203f2bda71771e9c2a10f5da60b8c747b28
Slav1Bak <players7@interia.pl>
2019-04-02 12:01:53 +0200

    widoki2

--- src/main/java/pl/sa/serwisaukcyjny/service/FrontService.java
+++ src/main/java/pl/sa/serwisaukcyjny/service/FrontService.java
@@ -1,13 +1,13 @@
 package pl.sa.serwisaukcyjny.service;
 
+import org.springframework.stereotype.Service;
 import pl.sa.serwisaukcyjny.controller.FrontController;
-//
-//@org.springframework.stereotype.Service
-//public class Service {
+@Service
+public class FrontService {
 //
 //FrontController controller;
 //
 //    public Service(FrontController controller) {
 //        this.controller = controller;
 //    }
-//}
+}
